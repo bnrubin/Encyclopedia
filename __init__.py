e09d58d4a603c8d00a2fde0cdf7cd966aca4b687
Dennis Kaarsemaker <dennis@ubuntu-nl.org>
2007-02-04 18:35:40 +0100

    Encyclopedia made neat.

--- __init__.py
+++ __init__.py
@@ -1,43 +1,37 @@
 ###
-# Copyright (c) 2006, Dennis Kaarsemaker
-# All rights reserved.
+# Copyright (c) 2006-2007 Dennis Kaarsemaker
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 ###
 
 """
-Add a description of the plugin (to be presented to the user inside the wizard)
-here.  This should describe *what* the plugin does.
+This plugin is a factoid encyclopedia and has Ubuntu/Debian package&file lookup
+funtionality
 """
 
 import supybot
 import supybot.world as world
 
-# Use this for the version of this plugin.  You may wish to put a CVS keyword
-# in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
-
-# XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
-
-# This is a dictionary mapping supybot.Author instances to lists of
-# contributions.
+__version__ = "2.1"
+__author__ = supybot.Author("Dennis Kaarsemaker","Seveas","dennis@kaarsemaker.net")
 __contributors__ = {}
-
-# This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Factoid plugin/download'
+__url__ = 'https://bots.ubuntulinux.nl'
 
 import config
 reload(config)
-import plugin_ng
-reload(plugin_ng)
-# Add more reloads here if you add third-party modules and want them to be
-# reloaded when this plugin is reloaded.  Don't forget to import them as well!
+import plugin
+reload(plugin)
 
 if world.testing:
     import test
 
-Class = plugin_ng.Class
+Class = plugin.Class
 configure = config.configure
-
-# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
